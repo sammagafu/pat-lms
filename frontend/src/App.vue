e74154dc4291661594a7da0584d7de04c300149c
Samuel Magafu <hi@sammagafu.com>
2022-10-17 19:11:38 +0300

    added courses views

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Sidebar from './components/Sidebar.vue';
 </script>
 
 <template>
-  <RouterView />
+<div class="flex flex-row">
+  <aside class="basis-1/5" aria-label="Sidebar">
+    <Sidebar />
+  </aside>
+  <div class="basis-4/5 px-16 bg-gray-50 dark:bg-gray-800">
+    <RouterView />
+  </div>
+</div>
 </template>
