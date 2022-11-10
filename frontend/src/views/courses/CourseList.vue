932789b70984d89fe8ce18b25b6aaa06e2b0b5d3
Samuel Magafu <hi@sammagafu.com>
2022-11-11 02:36:20 +0300

    added backend

--- frontend/src/views/courses/CourseList.vue
+++ frontend/src/views/courses/CourseList.vue
@@ -15,49 +15,77 @@
     </div>
 
     <div class="grid grid-cols-2 gap-8">
-        <figure class="md:flex bg-slate-100 rounded-sm p-8 md:p-0 dark:bg-slate-900">
-            <img class="w-auto h-24 md:w-48 md:h-auto mx-auto" src="@/assets/images/course/1.jpg" alt="">
-            <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-                <h2 class="dark:text-gray-200 font-bold">Course Heading</h2>
-                <blockquote>
-                    <p class="text-sm font-normal dark:text-gray-600">
-                        “Tailwind CSS is the only framework that I've seen scale
-                        on large teams. It’s easy to customize, adapts to any design,
-                        and the build size is tiny.”
-                    </p>
-                </blockquote>
-                <figcaption class="font-medium">
-                    <div class="text-sky-500 dark:text-sky-400">
-                        Sarah Dayan
-                    </div>
-                    <div class="text-slate-700 dark:text-slate-500">
-                        Staff Engineer, Algolia
-                    </div>
-                </figcaption>
-            </div>
-        </figure>
 
-        <figure class="md:flex bg-slate-100 rounded-sm p-8 md:p-0 dark:bg-slate-900">
-            <img class="w-auto h-24 md:w-48 md:h-auto mx-auto" src="@/assets/images/course/1.jpg" alt="">
-            <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-                <h2 class="dark:text-gray-200 font-bold">Course Heading</h2>
-                <blockquote>
-                    <p class="text-sm font-normal dark:text-gray-600">
-                        “Tailwind CSS is the only framework that I've seen scale
-                        on large teams. It’s easy to customize, adapts to any design,
-                        and the build size is tiny.”
-                    </p>
-                </blockquote>
-                <figcaption class="font-medium">
-                    <div class="text-sky-500 dark:text-sky-400">
-                        Sarah Dayan
-                    </div>
-                    <div class="text-slate-700 dark:text-slate-500">
-                        Staff Engineer, Algolia
-                    </div>
-                </figcaption>
+        <!-- <a href="#"
+            class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                src="@/assets/images/course/1.jpg" alt="">
+            <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
+                    acquisitions 2021</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
+                    acquisitions of 2021 so far, in reverse chronological order.</p>
             </div>
-        </figure>
+        </a> -->
+
+
+        <div class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <img class="rounded-t-lg" src="@/assets/images/course/1.jpg" alt="course name">
+            </a>
+            <div class="px-5 pb-5">
+                <a href="#">
+                    <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Course Detail</h5>
+                </a>
+                <div class="flex items-center mt-2.5 mb-5">
+                    <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <title>First star</title>
+                        <path
+                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                        </path>
+                    </svg>
+                    <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <title>Second star</title>
+                        <path
+                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                        </path>
+                    </svg>
+                    <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <title>Third star</title>
+                        <path
+                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                        </path>
+                    </svg>
+                    <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <title>Fourth star</title>
+                        <path
+                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                        </path>
+                    </svg>
+                    <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <title>Fifth star</title>
+                        <path
+                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                        </path>
+                    </svg>
+                    <span
+                        class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
+                </div>
+                <div class="flex justify-between items-center">
+                    <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                    <a href="#"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
+                        to cart</a>
+                </div>
+            </div>
+        </div>
+
+
 
     </div>
 </template>
