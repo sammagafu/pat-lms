b7f2daf67e9631876e5a12c2dda2c58eed0242c8
Samuel Magafu <hi@sammagafu.com>
2023-02-16 14:06:01 +0300

    fixed model issues

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from django.conf import settings
 from django.core.validators import RegexValidator
-from django.utils.translation import ugettext_lazy as _
-from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 from django_resized import ResizedImageField
 from django.utils import timezone
 
