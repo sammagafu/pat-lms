698b63b85a0988d2312df9e543fc0fec4503c935
Samuel Magafu <hi@sammagafu.com>
2023-02-16 14:14:07 +0300

    added fixed to updated gettext model

--- backend/coursecategory/models.py
+++ backend/coursecategory/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.text import slugify
